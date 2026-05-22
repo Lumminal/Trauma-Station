54ce36a80c5065684c0e9a4bd8be0b6d649f4ee2
Lumminal <spotifylefteris@gmail.com>
2026-05-22 22:23:03 +0300

    fire cone and meteors

---
+++ Content.Trauma.Shared/Lavaland/AshDrake/ActionFireConeSystem.cs
@@ -0,0 +1,42 @@
+﻿namespace Content.Trauma.Shared.Lavaland.AshDrake;
+
+public sealed partial class DirectionalEntitySpawnSystem : EntitySystem
+{
+    [Dependency] private INetManager _net = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<DirectionalEntitySpawnComponent, ActionDirectionEntitySpawnEvent>(OnAction);
+    }
+
+    private void OnAction(Entity<DirectionalEntitySpawnComponent> ent, ref ActionDirectionEntitySpawnEvent args)
+    {
+        if (_net.IsClient && !ent.Comp.Predicted)
+            return;
+
+        var performer = args.Performer;
+
+        var xform = Transform(performer);
+        var coords = xform.Coordinates;
+        var cardinalDir = xform.LocalRotation.GetCardinalDir();
+
+        switch (cardinalDir)
+        {
+            case Direction.North:
+                PredictedSpawnAtPosition(ent.Comp.North, coords);
+                break;
+            case Direction.East:
+                PredictedSpawnAtPosition(ent.Comp.East, coords);
+                break;
+            case Direction.South:
+                PredictedSpawnAtPosition(ent.Comp.South, coords);
+                break;
+            case Direction.West:
+                PredictedSpawnAtPosition(ent.Comp.West, coords);
+                break;
+        }
+    }
+}
+
---
+++ Content.Trauma.Shared/Lavaland/AshDrake/DirectionalEntitySpawnComponent.cs
@@ -0,0 +1,42 @@
+﻿using Content.Shared.Actions;
+
+namespace Content.Trauma.Shared.Lavaland.AshDrake;
+
+/// <summary>
+/// Spawns entities on top of an entity, different one depending on their cardinal direction
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class DirectionalEntitySpawnComponent : Component
+{
+    /// <summary>
+    /// The entity to spawn in north direction.
+    /// </summary>
+    [DataField]
+    public EntProtoId? North;
+
+    /// <summary>
+    /// The entity to spawn in east direction.
+    /// </summary>
+    [DataField]
+    public EntProtoId? East;
+
+    /// <summary>
+    /// The entity to spawn in west direction.
+    /// </summary>
+    [DataField]
+    public EntProtoId? West;
+
+    /// <summary>
+    /// The entity to spawn in south direction.
+    /// </summary>
+    [DataField]
+    public EntProtoId? South;
+
+    /// <summary>
+    /// Whether to predict the spawning
+    /// </summary>
+    [DataField]
+    public bool Predicted = true;
+}
+
+public sealed partial class ActionDirectionEntitySpawnEvent : InstantActionEvent;
